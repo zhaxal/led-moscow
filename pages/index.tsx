054d6fee9f4fff8620aa67c2a553076777cf3357
DumanBruh <dmarlambekov@gmail.com>
2023-01-31 08:11:00 +0600

    added mailru counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,6 +51,30 @@ export default function Home() {
         </div>
       </noscript>
       <Box>
+        <Script
+          id="mailCounter"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `var _tmr = window._tmr || (window._tmr = []);
+          _tmr.push({id: "3283202", type: "pageView", start: (new Date()).getTime()});
+          (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+          })(document, window, "tmr-code");`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://top-fwz1.mail.ru/counter?id=3283202;js=na"
+              style={{ position: "absolute", left: "-9999px;" }}
+              alt="Top.Mail.Ru"
+            />
+          </div>
+        </noscript>
         <Cover isMobile={matches} />
         <About isMobile={matches} />
         <Programs isMobile={matches} />
